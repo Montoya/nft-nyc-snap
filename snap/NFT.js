a3cbbeae913c184035c7f8140b7b85a168ddf170
hmalik88 <hassan.malik@consensys.net>
2022-06-22 06:18:09 -0400

    made some fixes, added snap_confirm to startWatching

--- snap/NFT.js
+++ snap/NFT.js
@@ -3,14 +3,14 @@ import { constructNotifications, fetchEvents } from "./utils";
 export async function addNFT (contractAddress, id) {
     const currState = await getNFTWatchList();
 
-    if (currState.contractAddress && currState.contractAddress.id) {
+    if (currState[contractAddress] && currState[contractAddress][id]) {
         throw new Error('Already watching this NFT.');
     }
 
-    const currContractState = currState.contractAddress ?? {};
+    const currContractState = currState[contractAddress] ?? {};
     const newState = {
         ...currState,
-        contractAddress: { ...currContractState, id: { lastRequested: 0 } },
+        [contractAddress]: { ...currContractState, [id]: { lastRequested: 0 } },
     };
 
     await wallet.request({
@@ -22,11 +22,11 @@ export async function addNFT (contractAddress, id) {
 export async function removeNFT(contractAddress, id) {
     const newState = await getNFTWatchList();
 
-    if (!newState.contractAddress || !newState.contractAddress.id) {
+    if (!newState[contractAddress] || !newState[contractAddress][id]) {
         throw new Error('Not currently watching this NFT.');
     }
 
-    delete newState.contractAddress.id;
+    delete newState[contractAddress][id];
 
     await wallet.request({
         method: 'snap_updateState',
@@ -41,11 +41,19 @@ export async function removeAllNFTs() {
     });
 }
 
-// read into how we're handling intervals in the execution environment as far as endowments
+let interval;
 
-export function startWatching() {
+export async function startWatching() {
+    if (interval) throw new Error('Already watching!');
+    const header = 'NFT watch request'
+    const message = 'Would you like to watch your list of NFTs?'
+    const res = await wallet.request({
+        method: 'snap_confirm',
+        params: [{ prompt: header, textAreaContent: message }],
+      });
+    if (!res) throw new Error('Watch request denied');
     const twoMinutes = 1000 * 60 * 2;
-    setInterval(async () => {
+    interval = setInterval(async () => {
         const eventsLists = await fetchEvents();
         const notifications = constructNotifications(eventsLists);
         await sendNotifications(notifications);
@@ -53,7 +61,9 @@ export function startWatching() {
 }
 
 export function stopWatching() {
-    clearInterval();
+    if (!interval) return;
+    clearInterval(interval);
+    interval = null;
 }
 
 export async function getNFTWatchList() {
